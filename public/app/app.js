b0478bae10f69bbc7263ec8af79b1d9223090c64
awfeese <feese83@gmail.com>
2015-08-07 10:30:28 -0500

    fixed routing issue

--- public/app/app.js
+++ public/app/app.js
@@ -6,6 +6,11 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
       auth: function(mvAuth) {
         return mvAuth.authorizeCurrentUserForRoute('admin');
       }
+    },
+    user: {
+      auth: function(mvAuth) {
+        return mvAuth.authorizeAuthenticatedUserForRoute();
+      }
     }
   }
 
@@ -21,14 +26,22 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
     .when('/admin/users', { templateUrl: '/partials/admin/user-list', 
       controller: 'mvUserListCtrl', resolve: routeRuleChecks.admin
     })
-    .when('/signup', { templateUrl: 'partials/account/signup', 
+    .when('/signup', { templateUrl: '/partials/account/signup', 
       controller: 'mvSignupCtrl'
+    })
+    .when('/profile', { templateUrl: '/partials/account/profile', 
+      controller: 'mvProfileCtrl', resolve: routeRuleChecks.user
+    })
+    .when('/courses', { templateUrl: '/partials/courses/course-list', 
+      controller: 'mvCourseListCtrl'
+    })
+    .when('/courses/:id', { templateUrl: '/partials/courses/course-details', 
+      controller: 'mvCourseDetailCtrl'
     });
 });
 
 angular.module('app').run(function($rootScope, $location) {
   $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection) {
-    console.log('redirect');
     if(rejection === 'not authorized') {
       $location.path('/');
     }
